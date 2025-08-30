598d38867bf454ebb369e97598885741e09faa59
kushal-p1699 <kushal.p1699@gmail.com>
2025-08-30 19:02:33 +0530

    Enhance NPMaster extension: add view containers, commands, and async Node project detection

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,46 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+/**
+ * This method is called when your extension is activated
+ *
+ * @param context - The extension context
+ */
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "npmaster" is now active!');
+  const updateContext = () => {
+    vscode.commands.executeCommand("setContext", "npmaster.state", "loading");
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "npmaster.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from NPMaster!");
+    const folders = vscode.workspace.workspaceFolders;
+    let hasNodeProject = false;
+
+    if (folders && folders.length > 0) {
+      for (const folder of folders) {
+        const packageJsonPath = path.join(folder.uri.fsPath, "package.json");
+        // Simulate async detection (replace with fs.promises if you want real async)
+        if (fs.existsSync(packageJsonPath)) {
+          hasNodeProject = true;
+          break;
+        }
+      }
     }
-  );
 
-  context.subscriptions.push(disposable);
+    setTimeout(() => {
+      vscode.commands.executeCommand(
+        "setContext",
+        "npmaster.state",
+        hasNodeProject ? "detected" : "notDetected"
+      );
+    }, 500);
+  };
+
+  // run once on activation
+  updateContext();
+
+  // Update if workspace changes
+  context.subscriptions.push(
+    vscode.workspace.onDidChangeWorkspaceFolders(updateContext)
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
